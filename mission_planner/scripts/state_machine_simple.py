432f008b4ba2a5a64ae055e5062ba7b49b24f74a
Donghee Han <hdh7485@naver.com>
2018-05-08 17:02:01 +0900

    Modify mission planner.

--- mission_planner/scripts/state_machine_simple.py
+++ mission_planner/scripts/state_machine_simple.py
@@ -5,19 +5,10 @@ import rospy
 import smach
 import smach_ros
 from keyboard.msg import Key
+import actionlib
+from action_with_smach.msg import MissionPlannerAction, MissionPlannerGoal, MissionPlannerResult, MissionPlannerFeedback
 
 mission_code = '\0'
-
-"""
-class KeyListener():
-    def __init__(self):
-        self.key_sub = rospy.Subscriber('keyboard/keydown', Key, self.keyboard_cb)
-
-    def keyboard_cb(self, data):
-        mission_code = char(data.code)
-        print(mission_code)
-        return mission_code
-"""
 
 #define state Foo
 class Foo(smach.State):
@@ -36,7 +27,7 @@ class Foo(smach.State):
         self.key_value = '\0'
         print("key value=")
         print(self.key_value)
-        while 1:
+        while True:
             if self.key_value == 'q':
                 return 'outcome3'
             elif self.key_value == '1':
@@ -46,13 +37,21 @@ class Foo(smach.State):
 
 #define state Bar
 class Bar(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome2'])
+        def __init__(self, client_name):
+                smach.State.__init__(self, outcomes=['outcome2'])
+                self.client_name = client_name
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state BAR')
-        rospy.sleep(2)
-        return 'outcome2'
+        def execute(self, userdata):
+                rospy.loginfo('Executing state BAR')
+                client = actionlib.SimpleActionClient(self.client_name, MissionPlannerAction)
+                client.wait_for_server()
+                goal = MissionPlannerGoal() 
+                goal.mission = 2
+                rospy.loginfo("send goal")
+                client.send_goal(goal)
+                client.wait_for_result()
+                rospy.loginfo('%s finish'%self.client_name)
+                return 'outcome2'
 
 #main
 def main():
@@ -63,14 +62,13 @@ def main():
 
     #Open the container
     with sm:
-        #smach.StateMachine.add('FOO', smach_ros.MonitorState("/keyboard/keydown", Key, keyboard_cb), transitions={'1':'BAR'})
-        #smach.StateMachine.add('BAR', smach_ros.MonitorState("/keyboard/keydown", Key, keyboard_cb), transitions={'2':'FOO'})
-        #Add states to the container
-        smach.StateMachine.add('MissionManager', Foo(), transitions={'outcome1':'Mission1', 'outcome2':'Mission2', 'outcome3':'outcome4'})
-        smach.StateMachine.add('Mission1', Bar(), transitions={'outcome2':'MissionManager'})
-        smach.StateMachine.add('Mission2', Bar(), transitions={'outcome2':'MissionManager'})
+        smach.StateMachine.add('MissionManager', Foo(), transitions={'outcome1':'dynamic_avoidance', 'outcome2':'uturn', 'outcome3':'follow_line'})
+        smach.StateMachine.add('dynamic_avoidance', Bar('dynamic_avoidance'), transitions={'outcome2':'MissionManager'})
+        smach.StateMachine.add('uturn', Bar('uturn'), transitions={'outcome2':'MissionManager'})
+        smach.StateMachine.add('follow_line', Bar('follow_line'), transitions={'outcome2':'MissionManager'})
 
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    gtk_disable_setlocale()
     sis.start()
 
     #Execute SMACH plan
@@ -82,4 +80,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
